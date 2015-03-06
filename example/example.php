587f6bf7e100746a8f0607d5ae86f7e49db6427c
Johannes Wachter <johannes.wachter@massiveart.at>
2015-03-06 14:28:53 +0100

    fixed example

--- example/example.php
+++ example/example.php
@@ -1,4 +1,4 @@
 <?php
-rs_sig_file('test/test-a.txt', 'test/test-a.sig');
-rs_delta_file('test/test-a.sig', 'test/test-b.txt', 'test/test-a.delta');
-rs_patch_file('test/test-a.txt', 'test/test-a.delta', 'test/test-a-new.txt');
+rs_sig_file('test-a.txt', 'test-a.sig');
+rs_delta_file('test-a.sig', 'test-b.txt', 'test-a.delta');
+rs_patch_file('test-a.txt', 'test-a.delta', 'test-a-new.txt');
